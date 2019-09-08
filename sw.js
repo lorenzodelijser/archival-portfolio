4b419680a088400055a9eefae0f779cd7ff06b12
Lorenzo de Lijser <>
2019-09-08 20:03:06 +0200

    Added cache-polyfill.js and Fixed A11y Problem.

---
+++ sw.js
@@ -0,0 +1,14 @@
+importScripts('./cache-polyfill.js');
+
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('portfolio').then(function (cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './css/style.min.css'
+            ]);
+        })
+    );
+});
