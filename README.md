0f7a460d7e3b91373150cdbbf34a196cffac0f1f
Lorenzo <29688706+lorenzodelijser@users.noreply.github.com>
2021-04-16 19:08:20 +0200

    Sprint cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# portfolio
+# Portfolio
+
+My personal website made in the beginning of my third year of my study communication and multimedia.
--- sw.js
+++
@@ -1,9 +0,0 @@
-importScripts("./cache-polyfill.js");
-
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("portfolio").then(function(cache) {
-      return cache.addAll(["./", "./index.html", "./css/style.min.css"]);
-    })
-  );
-});
